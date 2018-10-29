d742afabc920a45fd6581595b6b11e32718ca1da
catenocrypt <catenocrypt@users.noreply.github.com>
2018-10-29 10:41:05 +0100

    Adjust to new account representation (10e10 raw multiplier, v0.4).

--- service/account_helper.py
+++ service/account_helper.py
@@ -4,15 +4,8 @@ def fromRawToMikron(raw):
     # do the 10^30 multiplication in two steps to avoid non-zero trailing digits due to rounding error
     try:
         rawFloat = 0
-        if (len(raw) <= 10):
-            # short string, convert in one
-            rawFloat = float(raw)
-            rawFloat = rawFloat / float(1e30)
-        else:
-            # long string, simple cut the last 10 digits
-            raw = raw[:len(raw)-10]
-            rawFloat = float(raw)
-            rawFloat = rawFloat / float(1e20)
+        rawFloat = float(raw)
+        rawFloat = rawFloat / float(1e10)
     except:
         return "ERROR: Numercial conversion"
     return rawFloat
@@ -22,9 +15,7 @@ def fromMikronToRaw(mikron):
         mikronFloat = float(mikron)
     except:
         return "ERROR: Numercial conversion"
-    # do the 10^30 multiplication in two steps to avoid non-zero trailing digits due to rounding error
-    raw = math.floor(mikronFloat * float(1e20))
+    raw = math.floor(mikronFloat * float(1e10))
     rawStr = str(int(raw))
-    rawStr = rawStr + "0000000000"
     return rawStr
 
