7dac5bba619666d78da87fc00fd799b14268d02f
catenocrypt <catenocrypt@users.noreply.github.com>
2018-11-13 23:40:44 +0100

    Monitor: more detailed timestamp info.

--- monitor/db.py
+++ monitor/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 import time
+import datetime
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -31,7 +32,9 @@ def create_dbs():
     c, conn = connect()
 
     c.execute('''CREATE TABLE monitor
-            (timestamp long,
+            (time_sec long,
+            time_day_ordinal int,
+            time_iso text,
             no_nodes int,
             no_blocks long,
             no_frontiers long)''')
@@ -42,8 +45,11 @@ def create_dbs():
 
 def add_line(timestamp, no_nodes, no_blocks, no_frontiers):
     c, conn = connect()
+    date = datetime.datetime.fromtimestamp(timestamp).date()
     sql_command = "INSERT INTO monitor VALUES ('"+\
-        str(timestamp)+ "', '"+\
+        str(timestamp) + "', '"+\
+        str(date.toordinal()) + "', '"+\
+        date.isoformat() + "', '"+\
         str(no_nodes) + "', '" +\
         str(no_blocks) + "', '"+\
         str(no_frontiers) + "'"+\
@@ -57,7 +63,7 @@ def add_line(timestamp, no_nodes, no_blocks, no_frontiers):
 def get_all_lines():
     c, conn = connect()
 
-    c.execute("SELECT * FROM monitor ORDER BY timestamp ASC")
+    c.execute("SELECT * FROM monitor ORDER BY time_sec ASC")
     ret = c.fetchall()
     close(conn)
 
--- monitor/dump.py
+++ monitor/dump.py
@@ -4,6 +4,6 @@ def dump():
     lines = db.get_all_lines()
     #print(str(len(lines)) + " lines")
     for l in lines:
-        print(l['timestamp'], l['no_nodes'], l['no_blocks'], l['no_frontiers'], sep=', ')
+        print(l['time_sec'], l['time_day_ordinal'], "'" + l['time_iso'] + "'", l['no_nodes'], l['no_blocks'], l['no_frontiers'], sep=', ')
 
 dump()
