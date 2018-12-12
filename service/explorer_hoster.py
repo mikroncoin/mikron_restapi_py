48b88f30b4f636267325b0061356f3e685f03823
catenocrypt <catenocrypt@users.noreply.github.com>
2018-12-12 16:37:55 +0100

    No need for different service Root URL.

--- service/explorer_hoster.py
+++ service/explorer_hoster.py
@@ -8,15 +8,15 @@ def replaceServiceUrl(file):
 @route('/explorer/', method='GET')
 @route('/explorer', method='GET')
 def get_file_index():
-    #return static_file('index.html', root='explorer_web/')
-    f = open('explorer_web/index.html', 'r').read()
-    f = replaceServiceUrl(f)
-    return f
+    return static_file('index.html', root='explorer_web/')
+    #f = open('explorer_web/index.html', 'r').read()
+    #f = replaceServiceUrl(f)
+    #return f
 
 @route('/explorer/account/<account>', method='GET')
 def get_account(account):
     f = open('explorer_web/account.html', 'r').read()
-    f = replaceServiceUrl(f)
+    #f = replaceServiceUrl(f)
     f = f.replace('{{ACCOUNT_PLACEHOLDER}}', account)
     return f
 
