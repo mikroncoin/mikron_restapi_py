44c2b1e657c56b409ebc06875fc5bc916b0ac4ab
catenocrypt <catenocrypt@users.noreply.github.com>
2018-12-12 16:23:34 +0100

    Explorer: proto page for opening and account.

--- service/explorer_hoster.py
+++ service/explorer_hoster.py
@@ -1,9 +1,24 @@
 from bottle import post, request, response, get, route, static_file
 
+serviceBaseUrl = 'http://server2.mikron.io:8080';
+
+def replaceServiceUrl(file):
+    return file.replace('{{SERVICE_URL_PLACEHOLDER}}', serviceBaseUrl)
+
 @route('/explorer/', method='GET')
 @route('/explorer', method='GET')
 def get_file_index():
-    return static_file("index.html", root="explorer_web/")
+    #return static_file('index.html', root='explorer_web/')
+    f = open('explorer_web/index.html', 'r').read()
+    f = replaceServiceUrl(f)
+    return f
+
+@route('/explorer/account/<account>', method='GET')
+def get_account(account):
+    f = open('explorer_web/account.html', 'r').read()
+    f = replaceServiceUrl(f)
+    f = f.replace('{{ACCOUNT_PLACEHOLDER}}', account)
+    return f
 
 @route('/explorer/<filename>', method='GET')
 def get_file(filename):
